48cebcd9ae59b0879f8c74adfc3d22ee9574cfd7
Alex <173425531+AGarciaJr@users.noreply.github.com>
2024-10-23 11:39:29 -0500

    LoginRequest Class Created.

--- backend/src/main/java/com/vpalz/hotellosterrenos/dao/LoginRequest.java
+++ backend/src/main/java/com/vpalz/hotellosterrenos/dao/LoginRequest.java
@@ -1,4 +1,12 @@
 package com.vpalz.hotellosterrenos.dao;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    @NotEmpty(message = "Email is required.")
+    private String email;
+    @NotEmpty(message = "Password is required.")
+    private String password;
 }
