71816c0f7b6819414eaab84cbacd992ffa53b029
Alex <173425531+AGarciaJr@users.noreply.github.com>
2024-10-22 19:57:09 -0500

    Initial commit

---
+++ backend/src/main/java/com/vpalz/hotellosterrenos/HotelLosTerrenosApplication.java
@@ -0,0 +1,13 @@
+package com.vpalz.hotellosterrenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelLosTerrenosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelLosTerrenosApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/vpalz/hotellosterrenos/HotelLosTerrenosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpalz.hotellosterrenos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelLosTerrenosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
